ae2e9078f761f1962645d86182fe87ff1469fa31
fumiya-kume <fumiya.kume@hotmail.com>
2019-11-18 18:39:18 +0900

    add sample app module

---
+++ sample/waveparserapp/src/main/java/kuu/nagoya/waveparser/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package kuu.nagoya.waveparser.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kuu.nagoya.waveparser.app.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ sample/waveparserapp/src/main/java/kuu/nagoya/waveparser/app/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package kuu.nagoya.waveparser.app.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
