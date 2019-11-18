134a2943a5bc1b5b4f6f46c1599bde338682b639
fumiya-kume <fumiya.kume@hotmail.com>
2019-11-18 15:39:39 +0900

    add audio track extension module

---
+++ waveparser-audiotrack/src/main/java/kuu/nagoya/waveparser_audiotrack/AudioTrackExtensions.kt
@@ -0,0 +1,10 @@
+package kuu.nagoya.waveparser_audiotrack
+
+import android.media.AudioTrack
+
+fun AudioTrack.storeToWaveFile(
+    fileName: String
+){
+    val sampleRate = this.sampleRate
+    this.
+}
