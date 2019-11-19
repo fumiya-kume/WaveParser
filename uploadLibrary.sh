d6c21a68fad55d6d444b61a9bffdf4ee08ae8b4b
fumiya-kume <fumiya.kume@hotmail.com>
2019-11-19 18:40:56 +0900

    update shell

--- uploadLibrary.sh
+++ uploadLibrary.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-
 ./gradlew uploadArchives
 git add repository/
 git commit -m 'upload libraries archives'
 
-git branch -d gh-pages
-git checkout -b gh-pages
 git push -f --set-upstream origin gh-pages
