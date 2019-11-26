ef73d4a8bacc81e50b372133cdf3f72903f18460
fumiya-kume <fumiya.kume@hotmail.com>
2019-11-26 16:43:22 +0900

    update version up script

--- uploadLibrary.sh
+++ uploadLibrary.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+
+
 ./gradlew uploadArchives
 git add repository/
 git commit -m 'upload libraries archives'
 
-git push -f --set-upstream origin gh-pages
+git push -f --set-upstream origin master
